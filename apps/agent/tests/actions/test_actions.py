08b75b8a63471c6b3cf832544fd9564a58835443
ayush <ayushkumar2122.ak@gmail.com>
2026-09-10 10:34:10 +0530

    ci: pin Sage actions to latest commit
    Signed-off-by: ayush <ayushkumar2122.ak@gmail.com>

--- apps/agent/tests/actions/test_actions.py
+++ apps/agent/tests/actions/test_actions.py
@@ -8,7 +8,7 @@ ROOT = Path(__file__).parents[4]
 ACTIONS = ROOT / ".github" / "actions"
 WORKFLOW = ROOT / ".github" / "workflows" / "sage.yml"
 FULL_SHA_REFERENCE = re.compile(r"^[^@\s]+@[0-9a-f]{40}$")
-SAGE_ACTION_SHA = "8aa6875dcd8a576e67479706458ae8b0be39439a"
+SAGE_ACTION_SHA = "6b70347ca4a13eba736cae001f1bfa6e66e5561e"
 
 
 def test_composite_action_manifests_are_valid_and_pinned() -> None:
